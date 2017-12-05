2573a55eaac3104e3ebad7d2962ba00d13d91d62
lwz <619943379@qq.com>
2017-12-05 19:22:21 +0800

    Learn

---
+++ WZAuditory/WZAuditory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WZAuditory
+//
+//  Created by admin on 5/12/17.
+//  Copyright © 2017年 wizet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
